7bef2ce769d7894d231192fc540e71a25df6f98c
Dork-droid <dorcaskoskechepngetich@gmail.com>
2023-02-17 11:25:06 -0800

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 
 /**
@@ -7,15 +9,23 @@
  */
 int main (void)
 {
+	int i;
 	char l;
 
-	l = 'z';
-	while (l >= 'a')
+	i = 48 
+	while (i < 58)
 	{
 
+		putchar(i);
+		i++;
+	}
+	l = 'a';
+	while (l <= 'f')
+	{
 		putchar(l);
-		l--;
+		l++;
 	}
 	putchar('\n');
+
 	return (0);
 }
