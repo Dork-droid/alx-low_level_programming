30f17c785a50e3cb7686ccf7a30c9e7325c79559
Dork-droid <dorcaskoskechepngetich@gmail.com>
2023-02-23 11:27:18 -0800

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -2,9 +2,9 @@
 
 /**
  * largest_number - returns the largest of 3 numbers
- * @a: first interger
- * @b: second interger
- * @c: third interger
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
  * Return: largest number
  */
 
